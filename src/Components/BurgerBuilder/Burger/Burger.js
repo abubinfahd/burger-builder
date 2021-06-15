c8201fd19886f18978a3c45da7bbc292e7974708
Abu Bin Fahd <192040@ku.ac.bd>
2021-06-15 17:00:52 +0600

    show ingredient & controls

--- src/Components/BurgerBuilder/Burger/Burger.js
+++ src/Components/BurgerBuilder/Burger/Burger.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import Ingredient from '../Ingredient/Ingredient';
+import "./Burger.css";
 
 const Burger = props => {
+    let ingredientArr = props.ingredients.map(item => {
+        let amountArr = [...Array(item.amount).keys()];
+        //console.log(amountArr);
+        return amountArr.map(_ => {
+            return <Ingredient type={item.type} key={Math.random()} />
+        })
+    })
+        .reduce((arr, element) => {
+            return arr.concat(element);
+        }, []);
+    //console.log(ingredientArr);
+    if(ingredientArr.length === 0){
+        ingredientArr = <p>Please add some ingredients!</p>
+    }
+
+
     return(
-        <div>
+        <div className="Burger">
             <Ingredient type="bread-top" />
-            <Ingredient type="meat" />
-            <Ingredient type="cheese" />
-            <Ingredient type="salad" />
+            {ingredientArr}
             <Ingredient type="bread-bottom" />
         </div>
     )
--- src/Components/BurgerBuilder/BurgerBuilder.js
+++ src/Components/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,21 @@
 import React, {Component} from 'react';
 import Burger from "./Burger/Burger";
+import Controls from './Controls/Controls';
 
 export class BurgerBuilder extends Component {
+    state = {
+        ingredients : [
+            {type:'salad', amount: 0},
+            {type:'cheese', amount: 0},
+            {type:'meat', amount: 0},
+
+        ]
+    }
     render() {
         return( 
-            <div>
-                <Burger />
+            <div className="d-flex flex-md-row flex-column">
+                <Burger ingredients={this.state.ingredients} />
+                <Controls />
             </div>
         )
     }
