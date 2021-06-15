38fda48b1b0ac5c27fd39408e67c7a1fee445299
Abu Bin Fahd <192040@ku.ac.bd>
2021-06-15 09:33:25 +0600

    Navigation Bar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Main from "./Components/Main";
 
+
 function App() {
   return (
     <div>
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import './Header.css';
+import Logo from "../../assets/logo.png";
 
 const Header = () => {
-    return( 
-        <div>
-            <p>Header</p>
+    return (
+        <div className='Navigation'>
+            <Navbar style={{
+                backgroundColor: "#D70F64",
+                height: "70px",
+            }}>
+                <NavbarBrand href="/" className="mr-auto ml-md-5 Brand">
+                    <img src={Logo} alt="Logo" width="80px" />
+                </NavbarBrand>
+                <Nav className="mr-md-5">
+                    <NavItem>
+                        <NavLink href="#" className="NavLink">Something</NavLink>
+                    </NavItem>
+                </Nav>
+            </Navbar>
         </div>
     )
 }
